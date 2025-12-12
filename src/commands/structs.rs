5afac4d1e4167065c7402f978019dc158b03f03b
andrei399 <andreicristiantudor@gmail.com>
2025-12-12 14:11:29 +0200

    move command logic

---
+++ src/commands/structs.rs
@@ -0,0 +1,128 @@
+use std::str::{FromStr, SplitWhitespace};
+use std::sync::Arc;
+use papaya::{HashMap};
+use tokio::io::Result;
+
+use tokio::io::{self, AsyncReadExt};
+use tokio::net::tcp::OwnedReadHalf;
+
+struct Parser<'a> {
+    split: &'a mut SplitWhitespace<'a>,
+}
+impl Parser<'_> {
+    fn base_parse(&mut self, param_name: &str) -> Result<&str> {
+        let result = self.split.next().ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("-ERROR: {param_name} parameter is required."),
+            )
+        })?;
+        Ok(result)
+    }
+    fn parse_key(&mut self) -> Result<String> {
+        Ok(self.base_parse("KEY")?.to_string())
+    }
+    fn parse_value(&mut self) -> Result<String> {
+        Ok(self.base_parse("VALUE")?.to_string())
+    }
+    fn parse_seconds(&mut self) -> Result<u64> {
+        let param_name = "SECONDS";
+        let seconds = self.base_parse(param_name)?.parse::<u64>().map_err(|_| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "-ERROR: SECONDS parameter needs to be of type: u64",
+            )
+        })?;
+        Ok(seconds)
+    }
+}
+
+pub enum Commands {
+    Get {
+        key: String,
+    },
+    Set {
+        key: String,
+        value: String,
+    },
+    Setex {
+        key: String,
+        seconds: u64,
+        value: String,
+    },
+}
+
+impl Commands {
+    pub async fn parse_command(mut read_half: OwnedReadHalf) -> io::Result<Commands> {
+        let mut buffer = [0u8; 1024];
+        let n = read_half.read(&mut buffer).await?;
+
+        if n == 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::ConnectionAborted,
+                "-ERROR: Client sent no data",
+            ));
+        }
+
+        let input = String::from_utf8_lossy(&buffer[..n]);
+        let mut split = input.split_whitespace();
+        let command_type = split.next().ok_or_else(|| {
+            io::Error::new(io::ErrorKind::InvalidInput, "-ERROR: Missing command")
+        })?;
+        let mut parser = Parser { split: &mut split };
+        match command_type.to_uppercase().as_str() {
+            "GET" => {
+                Ok(Commands::Get {
+                    key: parser.parse_key()?,
+                })
+            }
+            "SET" => {
+                Ok(Commands::Set {
+                    key: parser.parse_key()?,
+                    value: parser.parse_value()?,
+                })
+            }
+            "SETEX" => {
+                Ok(Commands::Setex {
+                    key: parser.parse_key()?,
+                    seconds: parser.parse_seconds()?,
+                    value: parser.parse_value()?,
+                })
+            }
+            _ => Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "-ERROR: Unknown command.",
+            )),
+        }
+    }
+
+    pub async fn execute(&mut self, storage: &Arc<HashMap<String, String>>) -> Result<String> {
+        let db = storage.pin();
+        let result = match self {
+            Commands::Get { key } => db
+                .get(key)
+                .map(|val| format!("+{}\r\n", val))
+                .unwrap_or_else(|| format!("-ERROR: Key \"{key}\" not found").to_string()),
+            Commands::Set { key, value } => {
+                db.insert(key.clone(), value.clone());
+                String::from_str("+OK\r\n").unwrap()
+            },
+            Commands::Setex { key, seconds, value } => {
+                db.insert(key.clone(), value.clone());
+                drop(db);
+                let key_clone = key.clone();
+                let duration_seconds = *seconds;
+                let storage_clone = storage.clone();
+                tokio::spawn(async move {
+                    tokio::time::sleep(std::time::Duration::from_secs(duration_seconds)).await;
+                    let db = storage_clone.pin();
+                    db.remove(key_clone.as_str());
+                    drop(db);
+                });
+                String::from_str("+OK\r\n").unwrap()
+            }
+        };
+        Ok(result)
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,11 @@
 use papaya::HashMap;
-use std::str::FromStr;
 use std::sync::Arc;
-use std::time::Duration;
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
-use tokio::net::tcp::OwnedReadHalf;
+use tokio::io::{self, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use std::thread;
+use crate::commands::structs::Commands;
+pub mod commands;
 
 type Db = Arc<HashMap<String, String>>;
-
-enum Commands {
-    Get {
-        key: String,
-    },
-    Set {
-        key: String,
-        value: String,
-    },
-    Setex {
-        key: String,
-        seconds: u64,
-        value: String,
-    },
-}
-
-impl Commands {
-    async fn parse_command(mut read_half: OwnedReadHalf) -> io::Result<Commands> {
-        let mut buffer = [0u8; 1024];
-        let n = read_half.read(&mut buffer).await?;
-
-        if n == 0 {
-            return Err(io::Error::new(
-                io::ErrorKind::ConnectionAborted,
-                "-ERROR: Client sent no data",
-            ));
-        }
-
-        let input = String::from_utf8_lossy(&buffer[..n]);
-        let mut split = input.split_whitespace();
-        let command_type = split
-            .next()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "-ERROR: Missing command"))?;
-        match command_type.to_uppercase().as_str() {
-            "GET" => {
-                let key = split
-                    .next()
-                    .ok_or_else(|| {
-                        io::Error::new(io::ErrorKind::InvalidInput, "-ERROR: GET requires a key")
-                    })?
-                    .to_string();
-                Ok(Commands::Get { key })
-            }
-            "SET" => {
-                let key = split
-                    .next()
-                    .ok_or_else(|| {
-                        io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            "-ERROR: SET requires a key as the first parameter",
-                        )
-                    })?
-                    .to_string();
-                let value = split
-                    .next()
-                    .ok_or_else(|| {
-                        io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            "-ERROR: SET requires a value as the second parameter",
-                        )
-                    })?
-                    .to_string();
-                Ok(Commands::Set { key, value })
-            }
-            "SETEX" => {
-                let key = split
-                    .next()
-                    .ok_or_else(|| {
-                        io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            "-ERROR: SET requires a key as the first parameter",
-                        )
-                    })?
-                    .to_string();
-                let seconds = split
-                    .next()
-                    .ok_or_else(|| {
-                        io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            "-ERROR: SETEX requires 'seconds' as the second parameter",
-                        )
-                    })?
-                    .parse::<u64>()
-                    .map_err(|_| {
-                        io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            "-ERROR: SETEX 'seconds' parameter needs to be of type: u64",
-                        )
-                    })?;
-                let value = split
-                    .next()
-                    .ok_or_else(|| {
-                        io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            "-ERROR: SET requires a value as the third parameter",
-                        )
-                    })?
-                    .to_string();
-                Ok(Commands::Setex {
-                    key,
-                    seconds,
-                    value,
-                })
-            }
-            _ => Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "-ERROR: Unknown command.",
-            )),
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -140,41 +27,15 @@ async fn main() -> io::Result<()> {
 
 async fn handle_request(socket: TcpStream, storage: &Db) -> io::Result<()> {
     let (read_half, mut write_half) = socket.into_split();
-    let command = match Commands::parse_command(read_half).await {
+    let mut command = match Commands::parse_command(read_half).await {
         Ok(cmd) => cmd,
         Err(e) => {
-            let error_msg = format!("Error: {}\r\n", e);
-            write_half.write_all(error_msg.as_bytes()).await?;
+            write_half.write_all(format!("{}\r\n", e).as_bytes()).await?;
             return Ok(());
         }
     };
 
-    let response = {
-        let db = storage.pin();
-        match command {
-            Commands::Get { key } => db
-                .get(&key)
-                .map(|val| format!("+{}\r\n", val))
-                .unwrap_or_else(|| "-ERROR: Key \"{key}\" not found".to_string()),
-            Commands::Set { key, value } => {
-                db.insert(key, value);
-                String::from_str("+OK\r\n").unwrap()
-            },
-            Commands::Setex { key, seconds, value } => {
-                db.insert(key.clone(), value);
-                drop(db);
-                let storage_clone = storage.clone();
-                tokio::spawn(async move {
-                    thread::sleep(Duration::from_secs(seconds));
-                    let db = storage_clone.pin();
-                    db.remove(key.as_str());
-                    drop(db);
-                });
-                String::from_str("+OK\r\n").unwrap()
-            }
-        }
-    };
-    write_half.write_all(response.as_bytes()).await?;
+    write_half.write_all(command.execute(&storage).await?.as_bytes()).await?;
     write_half.flush().await?;
     Ok(())
 }
